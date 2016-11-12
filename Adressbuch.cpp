c059d1e3bb96e02551b78f757a393b20985aa855
Johann Hoffmann <johann.hoffmann96@t-online.de>
2016-11-12 13:55:33 +0100

    Initialer C++ Commit

---
+++ Adressbuch.cpp
@@ -0,0 +1,14 @@
+// Adressbuch.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
